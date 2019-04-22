356817fea3b912135d262b48fa21bfe7c4eae821
Matthew Retoff <matt.retoff@yahoo.com>
2019-04-22 18:36:28 -0400

    Set up home and profile page

--- online-checkers/src/App.js
+++ online-checkers/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Route from 'react-router-dom/Route';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Profile from './Profile';
+import Home from './Home';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route path="/profile" exact strict component={Profile} />
+          <Route path="/" exact strict component={Home} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
