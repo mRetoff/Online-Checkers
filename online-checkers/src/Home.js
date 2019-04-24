3d971f25163a7bf4018acd668bf1c45c2d324782
Matthew Retoff <matt.retoff@yahoo.com>
2019-04-23 21:16:19 -0400

    Logging in/out works correctly

--- online-checkers/src/Home.js
+++ online-checkers/src/Home.js
@@ -13,38 +13,37 @@ class Home extends Component {
 			currentUser: '',
 			email: '',
 			password: '',
+			username: '',
 		}
 		this.handleLogin = this.handleLogin.bind(this);
 		this.handleLogout = this.handleLogout.bind(this);
 	}
 
 	async handleLogin(ev) {
-		const {value: formValues} = await Swal.fire({
-		  title: 'Login',
-		  footer: '<a href>Not an existing user? Register</a>',
-		  html:
-		    '<input id="swal-input1" class="swal2-input" placeholder="Email">' +
-		    '<input id="swal-input2" class="swal2-input" placeholder="Password">',
-		  focusConfirm: false,
-		  preConfirm: () => {
-		    return [
-		      document.getElementById('swal-input1').value,
-		      document.getElementById('swal-input2').value
-		    ]
-		  }
+		const { value: formValues } = await Swal.fire({
+			title: 'Login',
+			footer: '<a href>Not an existing user? Register</a>',
+			html:
+				'<input id="swal-input1" class="swal2-input" placeholder="Email">' +
+				'<input id="swal-input2" class="swal2-input" placeholder="Password">',
+			focusConfirm: false,
+			preConfirm: () => {
+				return [
+					this.setState({
+						email: document.getElementById('swal-input1').value,
+						password: document.getElementById('swal-input2').value
+					})
+				]
+			}
 		})
 		//debug print out the string from input boxes
-		if (formValues) {
-			Swal.fire(JSON.stringify(formValues))
-		}
-
-		ev.preventDefault();
-		auth.signInWithEmailAndPassword(this.email, this.password)
+		//if (formValues) {
+		//Swal.fire(JSON.stringify(formValues))
+		//}
+		auth.signInWithEmailAndPassword(this.state.email, this.state.password)
 			.then(user => {
 				this.setState({ currentUser: user });
-				base.ref('users/' + user.uid).set({
-					online: true,
-				});
+				console.log("Logged in");
 			})
 			.catch(function (error) {
 				const errorMessage = "Invalid email/password";
@@ -54,22 +53,23 @@ class Home extends Component {
 
 	register() {
 		auth.createUserWithEmailAndPassword(this.email, this.password)
-		.then((user) => {
-			//Add user to database
-			base.ref('users/' + user.uid).set({
-				username: this.state.name,
-				email: this.state.email,
-				online: false,
+			.then((user) => {
+				//Add user to database
+				base.ref('users/' + user.uid).set({
+					username: this.state.name,
+					email: this.state.email,
+					wins: 0,
+					losses: 0,
+				});
+			})
+			.catch(function (error) {
+				var errorMessage = "Invalid email/password";
+				alert(errorMessage);
 			});
-		})
-		.catch(function (error) {
-			var errorMessage = "Invalid email/password";
-			alert(errorMessage);
-		});
 	}
 
 	setUserName(name) {
-		this.state.currentUser.updateProfile({
+		this.state.currentUser.user.updateProfile({
 			displayName: name,
 		})
 			.then(console.log("Success"))
@@ -77,34 +77,29 @@ class Home extends Component {
 	}
 
 	handleLogout(ev) {
-
 		Swal.fire({
-		  title: 'Are you sure you want to logout?',
-		  text: "You will have to log back in to play again!",
-		  type: 'question',
-		  showCancelButton: true,
-		  confirmButtonColor: '#3085d6',
-		  cancelButtonColor: '#d33',
-		  confirmButtonText: 'Yes, logout'
+			title: 'Are you sure you want to logout?',
+			text: "You will have to log back in to play again!",
+			type: 'question',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, logout'
 		}).then((result) => {
-		  if (result.value) {
-		    Swal.fire(
-		      'Logged Out',
-		      'Hope to see you back soon!',
-		      'success'
-		    )
-		  }
+			if (result.value) {
+				auth.signOut().then(() => {
+					this.setState({ currentUser: '' });
+					console.log("Logged out")
+				}).catch((error) => {
+					console.log(error)
+				});
+				Swal.fire(
+					'Logged Out',
+					'Hope to see you back soon!',
+					'success'
+				)
+			}
 		})
-
-		ev.preventDefault();
-		auth.signOut().then(function () {
-			base.ref('users/' + this.state.currentUser.uid).set({
-				online: false,
-			});
-			this.setState({ currentUser: '' });
-		}).catch(function (error) {
-			// An error happened.
-		});
 	}
 
 	componentDidMount() {
@@ -114,13 +109,13 @@ class Home extends Component {
 			}
 		});
 
-		var ref = base.ref("users/");
-		ref.on("value", function(snapshot) {
-		 snapshot.forEach(function(childSnapshot) {
-			var child = childSnapshot.val();
-			console.log(child);
-		 });
-		});
+		/*var ref = base.ref("users/");
+		ref.on("value", function (snapshot) {
+			snapshot.forEach(function (childSnapshot) {
+				var child = childSnapshot.val();
+				console.log(child);
+			});
+		});*/
 	}
 
 	render() {
@@ -129,16 +124,14 @@ class Home extends Component {
 				<div id="topBorder">
 					<h1 id="title">Online Checkers</h1>
 					<div id="buttons">
-						<button id="loginB" onClick={this.handleLogin}>
-							{this.state.user ? <NavLink exact to="/profile" activeClassName="active">Manage Profile</NavLink>
-								: "Login"}
-						</button>
+						<button id="loginB" onClick={this.handleLogin}>Login User 1</button>
+						<button id="loginB" onClick={this.handleLogin2}>Login User 2</button>
 						<button id="logoutB" onClick={this.handleLogout}>Logout</button>
 					</div>
 				</div>
 				<div id="page">
 					<div id="timer">
-
+					
 					</div>
 					<div id="board">
 
